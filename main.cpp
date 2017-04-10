a5d65b6ecf2cbb852f966e08ee0f7ae471f0c1cd
ankleanthous <antigoni.kleanthous.12@ucl.ac.uk>
2017-04-10 11:24:41 +0100

    initial commit
    code works for simple Laplace equation

---
+++ main.cpp
@@ -0,0 +1,56 @@
+// pLaplace equation (C++)
+// ======================
+
+#include <dolfin.h>
+#include "pLaplace.h"
+
+using namespace dolfin;
+
+// Source term (right-hand side)
+class Source : public Expression
+{
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    double dx = x[0] - 0.5;
+    double dy = x[1] - 0.5;
+    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
+  }
+};
+
+// Sub domain for Dirichlet boundary condition
+class DirichletBoundary : public SubDomain
+{
+  bool inside(const Array<double>& x, bool on_boundary) const
+  {
+    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS;
+  }
+};
+
+
+int main()
+{
+  // Create mesh and function space
+  auto mesh = std::make_shared<UnitSquareMesh>(32, 32);
+  auto V = std::make_shared<pLaplace::FunctionSpace>(mesh);
+
+  // Define boundary condition
+  auto u0 = std::make_shared<Constant>(0.0);
+  auto boundary = std::make_shared<DirichletBoundary>();
+  DirichletBC bc(V, u0, boundary);
+
+  // Define variational forms
+  pLaplace::BilinearForm a(V, V);
+  pLaplace::LinearForm L(V);
+  auto f = std::make_shared<Source>();
+  L.f = f;
+    
+  // Compute solution
+  Function u(V);
+  solve(a == L, u, bc);
+
+  // Save solution in VTK format
+  File file("pLaplace.pvd");
+  file << u;
+
+  return 0;
+}

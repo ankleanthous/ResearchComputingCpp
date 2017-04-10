ffce4c273144fb760d665cd4da3d3eedbb5ef869
ankleanthous <antigoni.kleanthous.12@ucl.ac.uk>
2017-04-11 00:36:26 +0100

    updated to ask user for input

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,6 +3,8 @@
 
 #include <dolfin.h>
 #include "pLaplace.h"
+#include <ufc.h>
+#include <iostream>
 
 using namespace dolfin;
 
@@ -29,12 +31,17 @@ class DirichletBoundary : public SubDomain
 
 int main()
 {
+    double input;
+    std::cout << "Please enter number of elements (e.g. 32):\n>";
+    std::cin >> input;
     // Create mesh and function space
-    auto mesh = std::make_shared<UnitSquareMesh>(64, 64);
+    auto mesh = std::make_shared<UnitSquareMesh>(input, input);
     auto V = std::make_shared<pLaplace::FunctionSpace>(mesh);
 
     // Define boundary condition
-    auto u0 = std::make_shared<Constant>(0.0);
+    std::cout << "Please enter constant boundary conditions (e.g. 3.0):\n>";
+    std::cin >> input;
+    auto u0 = std::make_shared<Constant>(input);
     auto boundary = std::make_shared<DirichletBoundary>();
     DirichletBC bc(V, u0, boundary);
     
@@ -43,9 +50,13 @@ int main()
     
     //Define Variatonal problem
     pLaplace::ResidualForm F(V);
-    //    auto f = std::make_shared<Constant>(1.0); 
-    auto f = std::make_shared<Source>();
-    auto p = std::make_shared<Constant>(3.0);
+    std::cout << "Please enter constant source function (e.g. 3.0) :\n>";
+    std::cin >> input;
+    auto f = std::make_shared<Constant>(input);
+//    auto f = std::make_shared<Source>();
+    std::cout << "Please enter constant p value (e.g. 4.0):\n>";
+    std::cin >> input;
+    auto p = std::make_shared<Constant>(input);
     auto epsilon = std::make_shared<Constant>(1.0e-5);
     F.p = p;
     F.u = u;
@@ -61,10 +72,10 @@ int main()
 
     
     // Solve Parameters
-    Parameters params("nonlinear_variational_solver");
-    Parameters newton_params("newton_solver");
-    newton_params.add("maximum_iterations", 1000);
-    //    newton_params.add("relative_tolerance", 1e-6);
+    Parameters params("nonlinear_variational_solver"); //to solve a nonlinear problem
+    Parameters newton_params("newton_solver"); //use the newton method to solve the problem
+    newton_params.add("maximum_iterations", 1000); //set number of maximum iterations
+    //    newton_params.add("relative_tolerance", 1e-6); //set relative tolerance instead of maximum iterations
     params.add(newton_params);
     
     // Compute solution

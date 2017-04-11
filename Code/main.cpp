8eac2cf8803f9dc7bbf721abceaf39cca6f9cb1d
ankleanthous <antigoni.kleanthous.12@ucl.ac.uk>
2017-04-11 01:46:29 +0100

    separated code
    -moved any necessary classes from the main.cpp into problem_classes.h
    -code works fine

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,85 +5,46 @@
 #include "pLaplace.h"
 #include <ufc.h>
 #include <iostream>
+#include "problem_classes.h"
+
+#include "catch.hpp"
 
 using namespace dolfin;
 
-// Source term (right-hand side)
-class Source : public Expression
-{
-  void eval(Array<double>& values, const Array<double>& x) const
-  {
-    double dx = x[0] - 0.5;
-    double dy = x[1] - 0.5;
-    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
-  }
-};
+//// Source term (right-hand side)
+//class Source : public Expression
+//{
+//  void eval(Array<double>& values, const Array<double>& x) const
+//  {
+//    double dx = x[0] - 0.5;
+//    double dy = x[1] - 0.5;
+//    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
+//  }
+//};
 
-// Sub domain for Dirichlet boundary condition
-class DirichletBoundary : public SubDomain
-{
-  bool inside(const Array<double>& x, bool on_boundary) const
-  {
-    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS;
-  }
-};
 
 
 int main()
 {
-    double input;
+    double no_elements;
     std::cout << "Please enter number of elements (e.g. 32):\n>";
-    std::cin >> input;
-    // Create mesh and function space
-    auto mesh = std::make_shared<UnitSquareMesh>(input, input);
-    auto V = std::make_shared<pLaplace::FunctionSpace>(mesh);
-
-    // Define boundary condition
+    std::cin >> no_elements;
+    
+    double u0_input;
     std::cout << "Please enter constant boundary conditions (e.g. 3.0):\n>";
-    std::cin >> input;
-    auto u0 = std::make_shared<Constant>(input);
-    auto boundary = std::make_shared<DirichletBoundary>();
-    DirichletBC bc(V, u0, boundary);
+    std::cin >> u0_input;
     
-    //Define solution function u
-    auto u = std::make_shared<Function>(V);
-    
-    //Define Variatonal problem
-    pLaplace::ResidualForm F(V);
+    double f_input;
     std::cout << "Please enter constant source function (e.g. 3.0) :\n>";
-    std::cin >> input;
-    auto f = std::make_shared<Constant>(input);
-//    auto f = std::make_shared<Source>();
+    std::cin >> f_input;
+    
+    double p_input;
     std::cout << "Please enter constant p value (e.g. 4.0):\n>";
-    std::cin >> input;
-    auto p = std::make_shared<Constant>(input);
-    auto epsilon = std::make_shared<Constant>(1.0e-5);
-    F.p = p;
-    F.u = u;
-    F.f = f;
-    F.epsilon = epsilon;
+    std::cin >> p_input;
     
-
-    //Create Jacobian J = F' to be used in the nonlinear solver
-    pLaplace::JacobianForm J(V,V);
-    J.u = u;
-    J.p = p;
-    J.epsilon = epsilon;
-
+    create_problem prob;
+    prob.set_values(no_elements, u0_input, f_input, p_input);
+    prob.problem_solver();
     
-    // Solve Parameters
-    Parameters params("nonlinear_variational_solver"); //to solve a nonlinear problem
-    Parameters newton_params("newton_solver"); //use the newton method to solve the problem
-    newton_params.add("maximum_iterations", 1000); //set number of maximum iterations
-    //    newton_params.add("relative_tolerance", 1e-6); //set relative tolerance instead of maximum iterations
-    params.add(newton_params);
-    
-    // Compute solution
-    solve(F == 0, *u, bc, J, params);
-
-    // Save solution in VTK format
-    File file("pLaplace.pvd");
-    file << *u;
-
     return 0;
 }
---
+++ Code/problem_classes.h
@@ -0,0 +1,79 @@
+#include <dolfin.h>
+#include "pLaplace.h"
+#include <ufc.h>
+#include <iostream>
+
+using namespace dolfin;
+
+class DirichletBoundary : public SubDomain
+{
+    bool inside(const Array<double>& x, bool on_boundary) const
+    {
+        return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS;
+    }
+};
+
+
+class create_problem {
+	double no_elements, u0_input, f_input, p_input;
+
+	public:
+		void set_values(double, double, double, double);
+
+	int problem_solver(){
+    // Create mesh and function space
+    auto mesh = std::make_shared<UnitSquareMesh>(no_elements, no_elements);
+    auto V = std::make_shared<pLaplace::FunctionSpace>(mesh);
+
+    // Define boundary condition
+    
+    auto u0 = std::make_shared<Constant>(u0_input);
+    auto boundary = std::make_shared<DirichletBoundary>();
+    DirichletBC bc(V, u0, boundary);
+    
+    //Define solution function u
+    auto u = std::make_shared<Function>(V);
+    
+    //Define Variatonal problem
+    pLaplace::ResidualForm F(V);
+    
+    auto f = std::make_shared<Constant>(f_input);
+//    auto f = std::make_shared<Source>(); // If a more complicated source function is desired please comment the above three commands and remove the comment on this one.
+
+    auto p = std::make_shared<Constant>(p_input);
+    auto epsilon = std::make_shared<Constant>(1.0e-5);
+    F.p = p;
+    F.u = u;
+    F.f = f;
+    F.epsilon = epsilon;
+    
+
+    //Create Jacobian J = F' to be used in the nonlinear solver
+    pLaplace::JacobianForm J(V,V);
+    J.u = u;
+    J.p = p;
+    J.epsilon = epsilon;
+
+    
+    // Solve Parameters
+    Parameters params("nonlinear_variational_solver"); //to solve a nonlinear problem
+    Parameters newton_params("newton_solver"); //use the newton method to solve the problem
+    newton_params.add("maximum_iterations", 1000); //set number of maximum iterations
+    //    newton_params.add("relative_tolerance", 1e-6); //set relative tolerance instead of maximum iterations
+    params.add(newton_params);
+    
+    // Compute solution
+    solve(F == 0, *u, bc, J, params);
+
+    // Save solution in VTK format
+    File file("pLaplace.pvd");
+    file << *u;
+};
+};
+
+void create_problem::set_values (double x, double y, double z, double w) {
+  no_elements = x;
+  u0_input = y;
+  f_input = z;
+  p_input = w;
+};

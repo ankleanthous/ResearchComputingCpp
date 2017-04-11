098078c1394187f7d49ea21e5f6f62856a7e3322
ankleanthous <antigoni.kleanthous.12@ucl.ac.uk>
2017-04-11 10:31:40 +0100

    created test for input

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -23,10 +23,49 @@ using namespace dolfin;
 //    rect.set_values (3,4);
 //    REQUIRE(rect.area() == 12);
 //}
+class manufactured_solution : public Expression
+{
+public:
+    void eval(Array<double>& values, const Array<double>& x) const
+    {
+        values[0] = 1 + x[0]*x[0] + x[1]*x[1];
+    }
+};
 
+//TEST_CASE("Positive p", "[niaou]"){
+//    double a;
+//    a = 5;
+//    REQUIRE(a<5);
+//}
 
-TEST_CASE("Positive p", "[niaou]"){
-    double a;
-    a = 5;
-    REQUIRE(a<5);
+TEST_CASE("Checking input is as assigned"){
+    create_problem prob;
+    double no_elements = 32;
+    double u0 = 3.0;
+    double f = -4;
+    double p = 0;
+    prob.set_values (no_elements, u0, f, p);
+    REQUIRE(prob.f_input == f);
+    REQUIRE(prob.no_elements == no_elements);
+    REQUIRE(prob.u0_input == u0);
+    REQUIRE(prob.p_input == p);
+}
+
+TEST_CASE("Manufactured Soln", "[sd]"){
+    create_problem prob;
+    double no_elements = 32;
+    double u0 = 3.0;
+    double f = -4;
+    double p = 0;
+    prob.set_values (no_elements, u0, f, p);
+    prob.problem_solver();
+    
+    auto mesh = std::make_shared<UnitSquareMesh>(no_elements, no_elements);
+    auto V = std::make_shared<pLaplace::FunctionSpace>(mesh);
+    auto u_exact = Function(V);
+    manufactured_solution ui;
+    u_exact.interpolate(ui);
+    REQUIRE(no_elements >0);
+//double uerror = norm(u_exact,std::string norm_type = "l2");
+    
 }

b4255f30928377dc4374f5bbbc7fea621b7a5912
ankleanthous <antigoni.kleanthous.12@ucl.ac.uk>
2017-04-11 09:19:46 +0100

    updates
    -updated CMakeLists.txt to include paths to the required libraries
    -updated Test.cpp to include paths to .h files in the Code folder
    -sample tests in Test.cpp not relevant to the problem; need to update to test the actual code

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -1,6 +1,32 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <dolfin.h>
+#include <iostream>
+#include "../Code/pLaplace.h"
+#include "../Code/problem_classes.h"
 
-TEST_CASE("My first test","[some group identifier]"){
-	int a = 5;
-	REQUIRE(a<6);
+using namespace dolfin;
+
+//unsigned int Factorial( unsigned int number ) {
+//    return number <= 1 ? number : Factorial(number-1)*number;
+//}
+//
+//TEST_CASE( "Factorials are computed", "[factorial]" ) {
+//    REQUIRE( Factorial(1) == 1 );
+//    REQUIRE( Factorial(2) == 2 );
+//    REQUIRE( Factorial(3) == 6 );
+//    REQUIRE( Factorial(10) == 3628800 );
+//}
+
+//TEST_CASE("Area","[xxx]"){
+//    Rectangle rect;
+//    rect.set_values (3,4);
+//    REQUIRE(rect.area() == 12);
+//}
+
+
+TEST_CASE("Positive p", "[niaou]"){
+    double a;
+    a = 5;
+    REQUIRE(a<5);
 }
